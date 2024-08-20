50adf0451154032a62eb03d430c2e1b91072ee1d
yuva4 <yuvavijay006@gmail.com>
2024-08-20 20:58:35 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,5 @@
+document.querySelector('.cta-btn').addEventListener('click', function() {
+    alert('Redirecting to Hotstar!');
+    // You can redirect the user to another page like this:
+    // window.location.href = "https://www.hotstar.com/";
+  });
